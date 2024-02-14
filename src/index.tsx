5ebbdf01180008ce031464b109b211c9e4b72374
Vadim <andrvalid@yandex.ru>
2024-02-14 20:26:33 +0500

    add prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes/routes";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./App"
+import { Provider } from "react-redux"
+import { store } from "./store/store"
+import { RouterProvider } from "react-router-dom"
+import { router } from "./routes/routes"
 
-
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router}/>
-        <App />
+      <RouterProvider router={router} />
+      <App />
     </Provider>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
