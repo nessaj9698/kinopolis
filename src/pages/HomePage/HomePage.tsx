e24c3e142420357c0a6b30c08296063e287504d0
Vadim <andrvalid@yandex.ru>
2024-02-16 23:03:51 +0500

    add site header

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,13 @@
+import { Header } from "../../components/layout/header/Header"
+import { Footer } from "../../components/layout/footer/Footer"
+
 export function HomePage() {
-  return <>HomePage</>
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
 }
 
 export default HomePage
