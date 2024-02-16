7b3276c0f9d94a63ee7db1a21f3137208fb5f3a5
Vadim <andrvalid@yandex.ru>
2024-02-16 23:22:51 +0500

    Add montserrat font and remove extra files

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-import "./index.css"
 import { Provider } from "react-redux"
 
+import "./App.css"
 import { store } from "./store/store"
 
 import App from "./App"
