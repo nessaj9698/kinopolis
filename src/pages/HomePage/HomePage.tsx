68ac991658a381226d48c086389db57c7b8df57a
Vadim <andrvalid@yandex.ru>
2024-02-17 02:22:05 +0500

    add homepage hero section

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,27 @@
 import { Header } from "../../components/layout/header/Header"
 import { Footer } from "../../components/layout/footer/Footer"
 
+import { Container } from "../../components/layout/container/Container"
+import { Input } from "../../components/input/Input"
+
+import s from "./Homepage.module.css"
+
 export function HomePage() {
   return (
     <>
       <Header />
+      <main>
+        <div className={s.heroSection}>
+          <Container>
+            <h1>Найди свой фильм</h1>
+            <Input
+              value="test"
+              handleChange={() => false}
+              placeholder="Введи название фильма"
+            />
+          </Container>
+        </div>
+      </main>
       <Footer />
     </>
   )
