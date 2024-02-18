c17078ceeb229516f69f2e1e6632e2bacae82b31
Vadim <andrvalid@yandex.ru>
2024-02-19 00:49:29 +0500

    add loader component

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,16 @@
+import s from './Loader.module.css'
+
+export const Loader = () => {
+  return (
+    <svg className={s.loader} viewBox="0 0 50 50">
+      <circle
+        className={s.loaderCircle}
+        cx="25"
+        cy="25"
+        r="20"
+        fill="none"
+        strokeWidth="5"
+      ></circle>
+    </svg>
+  )
+}
