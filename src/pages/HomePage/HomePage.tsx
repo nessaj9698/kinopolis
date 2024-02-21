f9a9841b0559a93370a326eb5f2573bda9f6811b
Vadim <andrvalid@yandex.ru>
2024-02-22 04:09:17 +0500

    refactoring homePage component with new other components

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,8 +4,7 @@ import { Header } from "../../components/layout/header/Header"
 import { Footer } from "../../components/layout/footer/Footer"
 
 import { Container } from "../../components/layout/container/Container"
-import { Input } from "../../components/input/Input"
-import { Button } from "../../components/button/Button"
+import { SearchForm } from "../../components/searchForm/SearchForm"
 import { CardRows } from "../../components/movieCards/CardRows"
 import { Loader } from "../../components/loader/Loader"
 
@@ -32,12 +31,7 @@ export function HomePage() {
         <section className={s.heroSection}>
           <Container>
             <h1>Найди свой фильм</h1>
-            <Input
-              value=""
-              handleChange={() => false}
-              placeholder="Введи название фильма"
-            />
-            <Button btnText="Искать" handleClick={() => false} />
+            <SearchForm/>
             {/* TODO: Поиск пока не работает, это лишь временная заглушка */}
           </Container>
         </section>
