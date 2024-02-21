959a68e67f06f5dfa5c2ec8396aa33c363b101fe
Vadim <andrvalid@yandex.ru>
2024-02-22 04:06:55 +0500

    refactoring input component to more flexibility

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,24 +1,34 @@
 import { ChangeEvent } from "react"
+import { useState } from "react"
 
 import s from "./Input.module.css"
 
 interface Props {
-  value: string
-  handleChange: (value: string) => void
+  value?: string
+  handleChange: (e: string) => void
   placeholder?: string
 }
 
-export const Input = ({ value, handleChange, placeholder = "" }: Props) => {
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    handleChange(e.target.value)
+export const Input = ({
+  value = "",
+  handleChange,
+  placeholder = "",
+}: Props) => {
+  const [inputValue, setInputValue] = useState<string>("")
+
+  const handleInputChange = (value: string) => {
+    handleChange(value)
+    setInputValue(value)
   }
 
   return (
     <input
       className={s.inputElement}
       type="text"
-      value={value}
-      onChange={handleInputChange}
+      value={value || inputValue}
+      onChange={(e: ChangeEvent<HTMLInputElement>) =>
+        handleInputChange(e.target.value)
+      }
       placeholder={placeholder}
     />
   )
