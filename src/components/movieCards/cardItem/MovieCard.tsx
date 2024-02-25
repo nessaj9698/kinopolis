346779ec3cfcee3ee14dc68c4471aca86a8d20e1
Vadim <andrvalid@yandex.ru>
2024-02-25 19:04:52 +0500

    minor refactoring cards components

--- src/components/movieCards/cardItem/MovieCard.tsx
+++ src/components/movieCards/cardItem/MovieCard.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 
 import { Movie } from "../../../types/Movies"
+import moviePosterDefault from "../../../assets/img/moviePoster.svg"
 
 import s from "./MovieCard.module.css"
 
@@ -8,7 +9,11 @@ export const MovieCard = ({ id, name, poster }: Movie) => {
   return (
     <Link to={`/movie/${id}`}>
       <article className={s.movieCard}>
-        <img src={poster.previewUrl} />
+        <img
+          src={
+            poster.previewUrl !== null ? poster.previewUrl : moviePosterDefault
+          }
+        />
         <h2>{name}</h2>
       </article>
     </Link>
