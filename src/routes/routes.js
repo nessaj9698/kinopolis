33789180db549e9fe5484da0e9e05944fd7bb2da
Vadim <andrvalid@yandex.ru>
2024-03-01 20:21:46 +0500

    add new pages to routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,7 +6,11 @@ import { fetchMoviesByQuery } from "../api/moviesApi"
 
 const LazyHomePage = lazy(() => import("../pages/HomePage/HomePage"))
 const LazySearchPage = lazy(() => import("../pages/SearchPage/SearchPage"))
-const LazyErrorPage = lazy(() => import("../pages/errorPage/ErrorPage"))
+const LazyErrorPage = lazy(() => import("../pages/ErrorPage/ErrorPage"))
+const LazyRegistrationPage = lazy(
+  () => import("../pages/RegistrationPage/RegistrationPage"),
+)
+const LazyLoginPage = lazy(() => import("../pages/LoginPage/LoginPage"))
 
 export const router = createBrowserRouter([
   {
@@ -19,8 +23,16 @@ export const router = createBrowserRouter([
     element: <LazySearchPage />,
     loader: async ({ params }) => {
       const query = params.query
-      const data = await fetchMoviesByQuery(query)
+      const data = await fetchMoviesByQuery(query, 20)
       return data
     },
+  },
+  {
+    path: "/registration",
+    element: <LazyRegistrationPage />,
+  },
+  {
+    path: "/login",
+    element: <LazyLoginPage />,
   },
 ])
