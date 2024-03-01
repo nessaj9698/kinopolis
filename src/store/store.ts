dba3ec5b48a97fa63960d966def93c3ee7ee6269
Vadim <andrvalid@yandex.ru>
2024-03-01 20:23:58 +0500

    create authSlice to manipulate loginization state

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit"
 
 import movies from "./moviesSlice"
+import authSlice from "./authSlice"
 
 export const store = configureStore({
   reducer: {
     movies: movies,
+    auth: authSlice,
   },
 })
 
