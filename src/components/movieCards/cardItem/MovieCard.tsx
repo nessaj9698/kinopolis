668207a4e2030a4258a9f6d2fdb4848bcb089557
Vadim <andrvalid@yandex.ru>
2024-03-03 03:46:51 +0500

    rewrite <MovieCard> component

--- src/components/movieCards/cardItem/MovieCard.tsx
+++ src/components/movieCards/cardItem/MovieCard.tsx
@@ -1,11 +1,31 @@
 import { Link } from "react-router-dom"
 
+import { useCallback } from "react"
+
 import { Movie } from "../../../types/Movies"
 import moviePosterDefault from "../../../assets/img/moviePoster.svg"
+import { HeartIcon } from "../../heartIcon/HeartIcon"
+import {
+  deleteUserDataFromDB,
+  setUserDataToDB,
+} from "../../../firebase/database/database"
+import { useAppSelector } from "../../../hooks/useAppSelector"
 
 import s from "./MovieCard.module.css"
 
 export const MovieCard = ({ id, name, poster }: Movie) => {
+  const userId = useAppSelector((state) => state.auth.user?.uid)
+  const addToFavourite = useCallback(() => {
+    if (userId) {
+      setUserDataToDB(userId, "likedMovies", id)
+    }
+  }, [userId])
+
+  const removeFromFavourite = useCallback(() => {
+    if (userId) {
+      deleteUserDataFromDB(userId, "likedMovies", id)
+    }
+  }, [])
   return (
     <Link to={`/movie/${id}`}>
       <article className={s.movieCard}>
@@ -15,6 +35,13 @@ export const MovieCard = ({ id, name, poster }: Movie) => {
           }
         />
         <h2>{name}</h2>
+        {userId && (
+          <HeartIcon
+            addToFavourite={addToFavourite}
+            removeFromFavourite={removeFromFavourite}
+            movieId={String(id)}
+          />
+        )}
       </article>
     </Link>
   )
