34ef167d747de6879ddf3cab1306959b0ad69a6c
Vadim <andrvalid@yandex.ru>
2024-03-04 23:14:24 +0500

    refactoring: delete useless suspense from <App> component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { RouterProvider } from "react-router-dom"
-import { Suspense } from "react"
-
-import { Loader } from "./components/loader/Loader"
 
 import { router } from "./routes/routes"
 import { getUserFromLS } from "./utils/localStorage"
@@ -14,13 +11,8 @@ function App() {
   if (user) {
     dispatch(setUser(user))
   }
-  // TODO: чутье подсказывает, что доставать юзера из ЛС нужно по-другому, но пока так
 
-  return (
-    <Suspense fallback={<Loader />}>
-      <RouterProvider router={router} />
-    </Suspense>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
