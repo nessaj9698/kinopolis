d25f6cb74de6897cb2869de76b4cb9fe3a2c79c4
Vadim <andrvalid@yandex.ru>
2024-03-07 00:59:52 +0500

    refactoring: minor <SearchForm> component changes

--- src/components/searchForm/SearchForm.tsx
+++ src/components/searchForm/SearchForm.tsx
@@ -2,10 +2,15 @@ import { useState, useEffect } from "react"
 
 import { useCallback } from "react"
 
-import { useNavigate } from "react-router-dom"
+import { toast } from "sonner"
 
 import { useAppDispatch } from "../../hooks/useAppDispatch"
-import { useAppSelector } from "../../hooks/useAppSelector"
+import {
+  useAppSelector,
+  fetchedSuggestSelector,
+  fetchedMoviesStatusSelector,
+  userSelector,
+} from "../../hooks/useAppSelector"
 
 import { Input } from "../input/Input"
 import { Button } from "../button/Button"
@@ -16,20 +21,33 @@ import { refreshMoviesState } from "../../store/moviesSlice"
 
 import { useNavigateToSearch } from "../../hooks/useNavigateToSearch"
 
+import { updateSearchHistoryLS } from "../../utils/localStorage"
+
+import { setUserDataToDB } from "../../firebase/database/database"
+
 import { SearchResults } from "./searchResults/SearchResults"
 
 import s from "./SearchForm.module.css"
 
 export const SearchForm = () => {
   const [searchQuery, setSearchQuery] = useState<string>("")
-  const fetchedMovies = useAppSelector((state) => state.movies.movies)
-  const fetchStatus = useAppSelector((state) => state.movies.fetchStatus)
+  const fetchedMovies = useAppSelector(fetchedSuggestSelector)
+  const fetchStatus = useAppSelector(fetchedMoviesStatusSelector)
+  const user = useAppSelector(userSelector)
   const dispatch = useAppDispatch()
-  const [debouncedQuery, setDebouncedQuery] = useDebounce<string>(
-    searchQuery,
-    1000,
-  )
+  const [debouncedQuery] = useDebounce<string>(searchQuery, 1000)
   const navigate = useNavigateToSearch(searchQuery)
+  const [isInputFocused, setIsInputFocused] = useState(false)
+
+  const handleFocus = () => {
+    setIsInputFocused(true)
+  }
+
+  const handleBlur = () => {
+    setTimeout(() => {
+      setIsInputFocused(false)
+    }, 150)
+  }
 
   const handleChange = useCallback(
     (query: string) => {
@@ -40,7 +58,15 @@ export const SearchForm = () => {
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
-    navigate()
+    if (searchQuery.length > 0) {
+      navigate()
+      updateSearchHistoryLS(searchQuery)
+      if (user?.uid) {
+        setUserDataToDB(user.uid, "history", searchQuery)
+      }
+    } else {
+      toast.error("Поле поиска не может быть пустым")
+    }
   }
 
   useEffect(() => {
@@ -56,13 +82,16 @@ export const SearchForm = () => {
       <Input
         handleChange={(query) => handleChange(query)}
         placeholder="Введи название фильма"
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        required={true}
       />
       <Button
         btnText="Искать"
         handleClick={(e: React.MouseEvent<HTMLButtonElement>) => handleClick(e)}
         type="submit"
       />
-      {fetchStatus !== null && (
+      {fetchStatus !== null && isInputFocused && (
         <SearchResults data={fetchedMovies} fetchStatus={fetchStatus} />
       )}
     </form>
