d256be71f342174e41d4fc3747d25ba6aef83844
Vadim <andrvalid@yandex.ru>
2024-03-06 23:53:14 +0500

    add type MovieApiData

--- src/types/Movies.ts
+++ src/types/Movies.ts
@@ -2,9 +2,15 @@ export type Movie = {
   id: number
   name: string
   poster: MoviePoster
+  year?: number
+  description?: string
 }
 
 type MoviePoster = {
   previewUrl: string
   url: string
+}
+
+export type MovieApiData = {
+  docs: Movie[]
 }
